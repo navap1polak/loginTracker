9ae55dd34c0d1d87fc56bdb7a8d6fa01c72c4207
navap1polak <nava.igal@gmail.com>
2020-09-12 17:51:58 +0300

    add docker files

--- src/main/java/com/imubit/loginTracker/controller/TrackerController.java
+++ src/main/java/com/imubit/loginTracker/controller/TrackerController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("api")
 @RestController
 public class TrackerController {
